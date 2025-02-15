a513435b19a0b30b5854c7498c38b2de7050d973
Romain Neutron <romain@neutron.io>
2025-02-15 19:22:30 +0100

    Add tags support

--- BlueskyTransport.php
+++ BlueskyTransport.php
@@ -208,6 +208,24 @@ final class BlueskyTransport extends AbstractTransport
             ];
         }
 
+        // partial/naive URL regex based on: https://github.com/bluesky-social/atproto/blob/main/packages/api/src/rich-text/util.ts
+        // https://github.com/bluesky-social/atproto/blob/main/packages/api/src/rich-text/detection.ts
+        $regex = '/(?:^|\s)(#[\p{L}\p{N}_]+)/u';
+        foreach ($this->getMatchAndPosition($text, $regex) as $match) {
+            $facets[] = [
+                'index' => [
+                    'byteStart' => $match['start'],
+                    'byteEnd' => $match['end'],
+                ],
+                'features' => [
+                    [
+                        '$type' => 'app.bsky.richtext.facet#tag',
+                        'tag' => $match['match'],
+                    ],
+                ],
+            ];
+        }
+
         return $facets;
     }
 
